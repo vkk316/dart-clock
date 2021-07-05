0c4a3378f1305c6416e616f941d7ba4ad5d46b70
Nuttanai Yaprajan <nuttanai03970@gmail.com>
2021-07-05 20:08:31 +0700

    bug

--- bin/clock.dart
+++ bin/clock.dart
@@ -1,5 +1,26 @@
+import 'clock_provider.dart';
+import 'state.dart';
+
 void main(List<String> arguments) {
-  arguments.forEach((element) {
-    print(element);
+  var input = [
+    'on 18 0',
+    'set',
+    'inc',
+    'set',
+    'inc',
+    'set',
+    'inc',
+    'inc',
+    'set'
+  ];
+
+  ClockProvider provider = ClockProvider(
+      initState: OnState(
+          hh: int.parse(input[0].split(' ')[1]),
+          mm: int.parse(input[0].split(' ')[2])));
+  input.removeAt(0);
+
+  input.forEach((e) {
+    print(provider.add(e).hh);
   });
 }
