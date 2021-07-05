1a6681f0bfe2d9a0f89829a48857d38d1ffba269
Nuttanai Yaprajan <nuttanai03970@gmail.com>
2021-07-05 20:15:10 +0700

    suck

--- bin/clock_provider.dart
+++ bin/clock_provider.dart
@@ -3,15 +3,16 @@ import 'state.dart';
 class ClockProvider {
   final ClockState? initState;
   ClockProvider({this.initState}) : _state = initState;
-  ClockState? _state;
+  ClockState? _state; // mutaBLE ไปก่อน
 
   ClockState add(String event) {
     if (event == 'set') {
-      return SetState(hh: _state!.hh, mm: _state!.mm);
+      print('this is setState');
+      _state = SetState(hh: _state!.hh, mm: _state!.mm);
     } else if (event == 'inc') {
-      return IncreseState(hh: _state!.hh! + 1, mm: _state!.mm);
-    } else {
-      return _state!;
+      print('this is increseState');
+      _state = IncreseState(hh: _state!.hh! + 1, mm: _state!.mm);
     }
+    return _state!;
   }
 }
